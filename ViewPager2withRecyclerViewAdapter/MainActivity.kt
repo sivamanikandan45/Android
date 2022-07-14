b6216ffbd11f93e2802d4d9577e595f45f991473
Sivamanikandan <72510000+sivamanikandan45@users.noreply.github.com>
2022-07-14 19:15:31 +0530

    Create MainActivity.kt

---
+++ ViewPager2withRecyclerViewAdapter/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.viewpager2example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val viewPager=findViewById<ViewPager2>(R.id.viewpager)
+        viewPager.adapter=ViewAdapter(listOf("Ohayo gosaimasu","Konnichiwa","Konbanwa"))
+    }
+}
